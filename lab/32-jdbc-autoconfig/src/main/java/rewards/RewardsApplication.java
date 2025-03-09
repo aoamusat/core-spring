56d0b34750453214df160f51fbfaf6d7e15e4c87
Akeem <aoamusat@gmail.com>
2025-03-09 17:20:12 +0100

    asaa

--- lab/32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
+++ lab/32-jdbc-autoconfig/src/main/java/rewards/RewardsApplication.java
@@ -3,6 +3,7 @@ package rewards;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // TODO-00 : In this lab, you are going to exercise the following:
 // - Understanding how auto-configuration is triggered in Spring Boot application
@@ -34,12 +35,11 @@ import org.springframework.boot.SpringApplication;
 
 // TODO-13 (Optional) : Follow the instruction in the lab document.
 //           The section titled "Build and Run using Command Line tools".
-
+@SpringBootApplication
 public class RewardsApplication {
     static final String SQL = "SELECT count(*) FROM T_ACCOUNT";
 
-    final Logger logger
-            = LoggerFactory.getLogger(RewardsApplication.class);
+    final Logger logger = LoggerFactory.getLogger(RewardsApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(RewardsApplication.class, args);
@@ -47,25 +47,26 @@ public class RewardsApplication {
 
     // TODO-04 : Let Spring Boot execute database scripts
     // - Move the SQL scripts (schema.sql and data.sql)
-    //   from `src/test/resources/rewards/testdb` directory
-    //   to `src/main/resources/` directory
+    // from `src/test/resources/rewards/testdb` directory
+    // to `src/main/resources/` directory
 
     // TODO-05 : Implement a command line runner that will query count from
-    //           T_ACCOUNT table and log the count to the console
+    // T_ACCOUNT table and log the count to the console
     // - Use the SQL query and logger provided above.
     // - Use the JdbcTemplate bean that Spring Boot auto-configured for you
     // - Run this application and verify "Hello, there are 21 accounts" log message
-    //   gets displayed in the console
+    // gets displayed in the console
 
     // TODO-07 (Optional): Enable full debugging in order to observe how Spring Boot
-    //           performs its auto-configuration logic
+    // performs its auto-configuration logic
     // - Follow TO-DO-07 in application.properties, then come back here.
     // - Run the application
     // - In the console output, find "CONDITIONS EVALUATION REPORT".
-    //   It represents the auto-configuration logic used by Spring Boot.
+    // It represents the auto-configuration logic used by Spring Boot.
     // - Search for "JdbcTemplateAutoConfiguration matched:" and
-    //   "DataSourceAutoConfiguration matched:". Note that each @Conditional*
-    //   represents a single conditional statement in the "JdbcTemplateAutoConfiguration"
-    //   and "DataSourceAutoConfiguration" classes.
+    // "DataSourceAutoConfiguration matched:". Note that each @Conditional*
+    // represents a single conditional statement in the
+    // "JdbcTemplateAutoConfiguration"
+    // and "DataSourceAutoConfiguration" classes.
 
 }
